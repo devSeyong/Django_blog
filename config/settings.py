4f498016bd5862cb94b4323e431d83e275b86574
seyong <choseyong0213@gmail.com>
2025-02-06 20:15:38 +0900

    Update. settings.py

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # 템플릿 디렉토리 추가
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
